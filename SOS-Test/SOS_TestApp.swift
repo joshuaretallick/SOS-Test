c3832bacc8a3f1b9fe28a17eeda73a275b353291
joshuaretallick <josh@old-empire.co.uk>
2021-01-20 11:28:59 +0000

    Initial Commit

---
+++ SOS-Test/SOS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOS_TestApp.swift
+//  SOS-Test
+//
+//  Created by Joshua Retallick on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SOS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
