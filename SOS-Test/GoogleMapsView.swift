0fe60730335929f790c4618025c53e8e8e18ee46
francescoFH <francescofacca@gmail.com>
2021-01-20 21:43:41 +0000

    User can see map with his/her current location

--- SOS-Test/GoogleMapsView.swift
+++ SOS-Test/GoogleMapsView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 import GoogleMaps
 
 struct GoogleMapsView: UIViewRepresentable {
-    
+    // 1
+    @ObservedObject var locationManager = LocationManager()
     private let zoom: Float = 15.0
     
+    // 2
     func makeUIView(context: Self.Context) -> GMSMapView {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: locationManager.latitude, longitude: locationManager.longitude, zoom: zoom)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         return mapView
     }
     
+    // 3
     func updateUIView(_ mapView: GMSMapView, context: Context) {
-        
+        //        let camera = GMSCameraPosition.camera(withLatitude: locationManager.latitude, longitude: locationManager.longitude, zoom: zoom)
+        //        mapView.camera = camera
+        mapView.animate(toLocation: CLLocationCoordinate2D(latitude: locationManager.latitude, longitude: locationManager.longitude))
     }
 }
 
